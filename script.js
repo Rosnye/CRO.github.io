a1998e3b091ec02250ba07fa8060f9752b982830
Dave <davd326at@gmail.com>
2023-05-19 00:17:51 -0600

    Agregado de funcion copiar en boton

--- script.js
+++ script.js
@@ -38,5 +38,25 @@ document.getElementById("encriptar").addEventListener("click", encriptarTexto);
 // Obtener referencia al botón de desencriptar y asignarle un controlador de eventos
 document.getElementById("desencriptar").addEventListener("click", desencriptarTexto);
 
+document.getElementById("copy").addEventListener("click", copiarTextoAlPortapapeles);
+
+function copiarTextoAlPortapapeles() {
+    /* Obtener el elemento textarea */
+    var textarea = document.getElementById('textarea-destino');
+  
+    /* Seleccionar el texto dentro del textarea */
+    textarea.select();
+    textarea.setSelectionRange(0, 99999); /* Para dispositivos móviles */
+  
+    /* Copiar el texto al portapapeles */
+    document.execCommand('copy');
+  
+    /* Deseleccionar el texto */
+    textarea.blur();
+  
+    /* Mostrar una notificación o realizar alguna otra acción */
+    //alert('Texto copiado al portapapeles');
+  }
+
 /*Este código define dos funciones: encriptarTexto() y desencriptarTexto(), 
 que se ejecutan cuando los botones "encriptar" y*/
